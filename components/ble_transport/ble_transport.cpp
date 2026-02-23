a6e9409c159a5ca7a6a758a044c33ed75377ca41
OBrunoW <brunowilsoneng@gmail.com>
2026-02-23 13:11:54 -0300

    fix: ajuste de pinos UART, partitions e roteamento no gateway

--- components/ble_transport/ble_transport.cpp
+++ components/ble_transport/ble_transport.cpp
@@ -161,7 +161,7 @@ void BLETransport::handle_button_write(const uint8_t *data, uint16_t len)
     out.type   = Protocol::PacketType::EVENT;
     out.method = "BUTTON_EVENT";
     out.route.src  = std::string(btn_id);
-    out.route.dst  = "gateway";
+    out.route.dst  = "server";
 
     // Body JSON com todos os campos relevantes
     char body_buf[256];
